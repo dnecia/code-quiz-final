23c9b14912a88c8d9ef5a550e3214e6b40fea2f3
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-13 23:02:25 -0500

    updating html js file with go back feature

--- HighScores.js
+++ HighScores.js
@@ -11,3 +11,18 @@ clear.addEventListener("click", function () {
 });
 
 //local storage
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+    for (var i = 0; i < allScores.length; i++) {
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+    }
+}
+
+//click for index page 
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
